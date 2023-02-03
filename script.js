d9ad8bc596abcab927fc4a94ce741c557c9f57ba
Gustavo Rodriguez <gustavodrodriguez93@gmail.com>
2023-02-03 21:56:05 +0100

    Profile site

---
+++ script.js
@@ -0,0 +1,17 @@
+const navLinks = document.querySelectorAll('.nav-item')
+
+const menuToggle = document.getElementById('navbarSupportedContent')
+
+const bsCollapse = new bootstrap.Collapse(menuToggle)
+
+const Nav= document.getElementsByClassName("Nav")
+
+const boton= document.querySelector("button")
+
+let exapnsion= boton["aria-expanded"]
+
+navLinks.forEach((link) => {
+
+    link.addEventListener('click', () => { bsCollapse.toggle() })
+    
+})
